7241809572730ca437b0ba2d441e0bd8ab09c9a0
Marius Mitrofan <marius.mitrofan@netbears.ro>
2019-10-25 09:50:34 +0300

    Zone is now deprecated for "google_container_engine_versions" data resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,6 @@ version of the Cloud Console form you would use to create a Kubernetes Engine cl
 
 # Gets the current version of Kubernetes engine
 data "google_container_engine_versions" "gke_version" {
-  zone = var.zone
 }
 
 // https://www.terraform.io/docs/providers/google/d/google_container_cluster.html
