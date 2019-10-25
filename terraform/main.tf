2eceb5b2c8a39ca1958e8e5008651877b4e9662e
Marius Mitrofan <marius.mitrofan@netbears.ro>
2019-10-25 09:57:48 +0300

    Use location instead of zone

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,7 @@ version of the Cloud Console form you would use to create a Kubernetes Engine cl
 
 # Gets the current version of Kubernetes engine
 data "google_container_engine_versions" "gke_version" {
+    location   = var.zone
 }
 
 // https://www.terraform.io/docs/providers/google/d/google_container_cluster.html
